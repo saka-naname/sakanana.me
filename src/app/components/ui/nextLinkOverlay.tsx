341cfbaad806a26a675ef6c870b83270fb4ccf30
saka-naname <reisuicupsoup@gmail.com>
2023-11-26 17:25:19 +0900

    NextLinkOverlayがclassNameを受け取れるように

--- src/app/components/ui/nextLinkOverlay.tsx
+++ src/app/components/ui/nextLinkOverlay.tsx
@@ -1,32 +1,27 @@
 import Link from "next/link";
 import { isLocalURL } from "next/dist/shared/lib/router/utils/is-local-url";
 import { LinkOverlayStyles, linkOverlay } from "@/../styled-system/patterns";
+import { cx } from "@/../styled-system/css";
 
 type NextLinkOverlayProps = LinkOverlayStyles & {
   children?: React.ReactNode;
+  className?: string;
   href: string;
   title?: string;
 };
 
 export default function NextLinkOverlay(props: NextLinkOverlayProps) {
   const linkOverlayStyles: LinkOverlayStyles = props;
+  const className = cx(linkOverlay(linkOverlayStyles), props.className);
   return (
     <>
       {isLocalURL(props.href) ? (
-        <Link
-          className={linkOverlay(linkOverlayStyles)}
-          href={props.href}
-          title={props.title}
-        >
+        <Link className={className} href={props.href} title={props.title}>
           {props.children}
         </Link>
       ) : (
         // <Link>コンポーネントでGitHub等のOpen-In-App機能が正しく動作しない問題を回避
-        <a
-          className={linkOverlay(linkOverlayStyles)}
-          href={props.href}
-          title={props.title}
-        >
+        <a className={className} href={props.href} title={props.title}>
           {props.children}
         </a>
       )}
