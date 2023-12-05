9fc5d27534ad06c24017a1c8dd96b511ad9ce718
saka-naname <reisuicupsoup@gmail.com>
2023-12-05 19:22:17 +0900

    やっぱり不要になったimportを削除

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,26 +1,8 @@
-import {
-  Noto_Sans_JP,
-  Yomogi as GFYomogi,
-  Cabin_Sketch,
-} from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 
 export const NotoSansJP = Noto_Sans_JP({
   weight: ["400", "500", "700"],
   display: "swap",
   variable: "--font-noto-sans-jp",
-  subsets: ["latin"],
-});
-
-export const Yomogi = GFYomogi({
-  weight: ["400"],
-  display: "block",
-  variable: "--font-yomogi",
-  subsets: ["latin"],
-});
-
-export const CabinSketch = Cabin_Sketch({
-  weight: ["700"],
-  display: "block",
-  variable: "--font-cabin-sketch",
   subsets: ["latin"],
 });
